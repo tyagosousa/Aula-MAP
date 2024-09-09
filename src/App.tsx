93d0185b37abad4b223b91df701647f83cdac421
Tyago Sousa <thiagosousaa84@gmail.com>
2024-09-09 14:55:15 -0300

    salvando

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,11 @@ import { useState } from 'react';
 
 function App() {
 
-  const [nome, setNome] = useState(' ');
-  const [mensagem, setMensagem] = useState(''); 
-
-  const handleNomeChange = (event) => {
-    setNome(event.target.value); // Atualiza valor1 com o valor do input
-  }
-
-  const handleSubmit = () => {
-    setMensagem(`Olá, ${nome}`); // Define a mensagem para exibir
-  };
-
+  const [nome, setNome] = useState('Thiago');
 
   return (
     <>
-
-
-        <input 
-          type="text" 
-          value={nome} 
-          onChange={handleNomeChange} 
-        />
-      
-          
-      <div><button onClick={handleSubmit}>Enviar</button> </div>
-      <div></div>
-      <p>{mensagem}</p>
-
- 
-      
-
-
+        <h1>{nome}</h1>
     </>
   );
 }
